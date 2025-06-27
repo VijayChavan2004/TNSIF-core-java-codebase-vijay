165a05dae06043b0c5f2c5ff8fc63f19afcc074c
DELL <DELL@DESKTOP-IVCJQIH>
2025-06-27 17:53:20 +0530

    hi

---
+++ demo/src/com/tnsif/demo/firstdemo.java
@@ -0,0 +1,11 @@
+package com.tnsif.demo;
+
+public class firstdemo {
+
+	public static void main(String[] args) {
+		System.out.println("..hi there , this is second trial of pushing code to github..");
+		// TODO Auto-generated method stub
+
+	}
+
+}
