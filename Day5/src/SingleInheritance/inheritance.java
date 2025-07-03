7bac81b12ab1ec7eeb99c07af6e266877c2bb02f
DELL <DELL@DESKTOP-IVCJQIH>
2025-07-03 22:32:34 +0530

    day5 codes...

---
+++ Day5/src/SingleInheritance/inheritance.java
@@ -0,0 +1,13 @@
+package SingleInheritance;
+
+public class inheritance {
+
+	public static void main(String[] args) {
+	
+		
+		Student student = new Student("Vijay", "123456789", "Pune", 10203040L, 8010, "Ashirwad");
+
+		System.out.println(student);
+	}
+
+}
