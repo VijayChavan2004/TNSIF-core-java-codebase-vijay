2829a522babd84b754bd0cb9963ee1eb3594ffe6
DELL <DELL@DESKTOP-IVCJQIH>
2025-07-06 20:51:57 +0530

    Day6 codes...

--- Day6/src/finalmethod/finalclass.java
+++ Day6/src/finalmethod/finalclass.java
@@ -3,7 +3,7 @@ package finalmethod;
 public class finalclass {
 	void show()
 	{
-		System.out.println("final class can not be inherited ");
+		System.out.println("final class can not be inherited. ");
 	}
 
 }
